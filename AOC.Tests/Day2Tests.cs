8167831a5a49ca098fb03e1e531a9d00ce045348
Regan Baty <regan@reganbaty.com>
2021-12-02 19:38:37 +1100

    Fixed test

--- AOC.Tests/Day2Tests.cs
+++ AOC.Tests/Day2Tests.cs
@@ -9,11 +9,11 @@ public class Day2Tests
     {
         var day = new Day2();
         var result = day.Run(@"forward 5
-        down 5
-        forward 8
-        up 3
-        down 8
-        forward 2").Result;
+down 5
+forward 8
+up 3
+down 8
+forward 2").Result;
         Assert.Equal(150, result.Part1);
         Assert.Equal(900, result.Part2);
     }
